bfe90f1224fe41dec71c717189554a0443c5edbd
rocioizq <rocioizquierdo98@gmail.com>
2025-05-15 19:03:00 +0200

    arreglando error de imagenes

--- 02 - Junior/social-media-dashboard/src/componentes/dashboardData.js
+++ 02 - Junior/social-media-dashboard/src/componentes/dashboardData.js
@@ -4,103 +4,103 @@ const dashboardData = {
 
   accounts: [
     {
-      image: "/public/images/icon-facebook.svg",
+      image: "/images/icon-facebook.svg",
       username: "@nathanf",
       platform: "facebook",
       followers: 1987,
       todayChange: 12,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-twitter.svg",
+      image: "/images/icon-twitter.svg",
       username: "@nathanf",
       platform: "twitter",
       followers: 1044,
       todayChange: 99,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-instagram.svg",
+      image: "/images/icon-instagram.svg",
       username: "@realnathanf",
       platform: "instagram",
       followers: "11k",
       todayChange: 1099,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-youtube.svg",
+      image: "/images/icon-youtube.svg",
       username: "Nathan F.",
       platform: "youtube",
       subscribers: 8239,
       todayChange: 144,
-      arrow: "/public/images/icon-down.svg"
+      arrow: "/images/icon-down.svg"
     }
   ],
 
   overviewToday: [
     {
-      image: "/public/images/icon-facebook.svg",
+      image: "/images/icon-facebook.svg",
       platform: "facebook",
       metric: "Page Views",
       value: 87,
       changePercent: 3,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-facebook.svg",
+      image: "/images/icon-facebook.svg",
       platform: "facebook",
       metric: "Likes",
       value: 52,
       changePercent: 2,
-      arrow: "/public/images/icon-down.svg"
+      arrow: "/images/icon-down.svg"
     },
     {
-      image: "/public/images/icon-instagram.svg",
+      image: "/images/icon-instagram.svg",
       platform: "instagram",
       metric: "Likes",
       value: 5462,
       changePercent: 2257,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-instagram.svg",
+      image: "/images/icon-instagram.svg",
       platform: "instagram",
       metric: "Profile Views",
       value: "52k",
       changePercent: 1375,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-twitter.svg",
+      image: "/images/icon-twitter.svg",
       platform: "twitter",
       metric: "Retweets",
       value: 117,
       changePercent: 303,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-twitter.svg",
+      image: "/images/icon-twitter.svg",
       platform: "instagram",
       metric: "Likes",
       value: 507,
       changePercent: 553,
-      arrow: "/public/images/icon-up.svg"
+      arrow: "/images/icon-up.svg"
     },
     {
-      image: "/public/images/icon-youtube.svg",
+      image: "/images/icon-youtube.svg",
       platform: "youtube",
       metric: "Likes",
       value: 107,
       changePercent: 19,
-      arrow: "/public/images/icon-down.svg"
+      arrow: "/images/icon-down.svg"
     },
     {
-      image: "/public/images/icon-youtube.svg",
+      image: "/images/icon-youtube.svg",
       platform: "youtube",
       metric: "Total Views",
       value: 1407,
       changePercent: 12,
-      arrow: "/public/images/icon-down.svg"
+      arrow: "/images/icon-down.svg"
     }
   ]
 };
